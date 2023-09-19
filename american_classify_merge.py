a08b8a9d7d226bda99e9c589a10d08c059da3be3
Quinn <93222595+qsluzenski@users.noreply.github.com>
2023-09-19 15:57:53 -0500

    Add files via upload

---
+++ american_classify_merge.py
@@ -0,0 +1,56 @@
+import json
+import csv
+
+json_file_path1 = "american_classify_file_name.json"
+json_file_path2 = "american_classify_result.json"
+output_file_path = "american_classify_merged_output.json"  # Specify the desired output file path
+csv_file_path = "american_classify_merged.csv"
+
+# Load content from JSON files
+with open(json_file_path1, "r") as file1, open(json_file_path2, "r") as file2:
+    data1 = json.load(file1)
+    data2 = json.load(file2)
+
+# Merge dictionaries
+merged_data = []
+
+# Iterate through the dictionaries in both lists
+for dict1, dict2 in zip(data1, data2):
+    merged_dict = {}
+    
+    # Update with the content from the first JSON file
+    for key, value in dict1.items():
+        merged_dict[key] = value
+    
+    # Update with the content from the second JSON file
+    for key, value in dict2.items():
+        merged_dict[key] = value
+    
+    merged_data.append(merged_dict)
+
+# Write merged data to a new JSON file
+with open(output_file_path, "w") as output_file:
+    json.dump(merged_data, output_file, indent=4)
+
+print("Merged data written to", output_file_path)
+
+
+# Load content from JSON file
+with open(output_file_path, "r") as json_file:
+    data = json.load(json_file)
+
+header = ["FILENAME", "FILENAME2", "Title", "User-generated title", "Date", "Publisher", "State", "Country", "City", "Body of water", "Unidentified", "Category", "Subject 1", "Subject 2", "Subject 3", "Other information"]
+
+# Write JSON data to CSV
+with open(csv_file_path, "w", newline="") as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=header)
+    
+    # Write the header row
+    csv_writer.writeheader()
+    
+    # Write each dictionary as a row in the CSV file
+    for entry in data:
+        if entry["retired"] is not None:  # Only write if "retired" is not null
+            csv_writer.writerow(entry)
+
+print("JSON data converted to CSV:", csv_file_path)
---
+++ american_classify_p_t_data_parser.py
@@ -0,0 +1,103 @@
+import csv
+import json
+
+def save_dict_to_json(data_dict, file_path):
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(data_dict, json_file, indent=4)
+
+def find_task_values(csv_file_path):
+    # We will parse each row and push the results into this list
+    output = [] 
+    justJson = []
+    with open(csv_file_path, 'r', encoding='utf-8', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row in csv_reader:
+            try:
+                if row[5] == "American Scenes":
+                        data_dict = json.loads(row[11]) # Assuming 0-based indexing, so 11 is the 12th column
+                        if isinstance(data_dict, list):
+                            justJson.append(data_dict)
+                            # this is the row's output, which will be pushed into the final list
+                            outpObj = {}
+                            for i in data_dict:
+                                if i['task'] == 'T0':
+                                    for j in i["value"]:
+                                        if j['task'] == 'T1':
+                                            for d in j["value"]:
+                                                if d["tool_label"] == 'Caption/Title':
+                                                    if "details" in d:
+                                                        for v in d["details"]:
+                                                            if "value" in v:
+                                                                outpObj["Title"] = v["value"]
+                                        elif j['task'] == 'T2' and len(j["value"]) > 0:
+                                            outpObj["User-generated title"] = j["value"]
+                                if j['task'] == 'T3':
+                                            for d in j["value"]:
+                                                if d["tool_label"] == 'Production number':
+                                                    if "details" in d:
+                                                        for v in d["details"]:
+                                                            if "value" in v:
+                                                                outpObj["Production number"] = v["value"]
+                                                elif d["tool_label"] == 'Publisher':
+                                                    if "details" in d:
+                                                        for v in d["details"]:
+                                                            if "value" in v:
+                                                                outpObj["Publisher"] = v["value"]
+                                                elif d["tool_label"] == 'Date':
+                                                    if "details" in d:
+                                                        for v in d["details"]:
+                                                            if "value" in v:
+                                                                outpObj["Date"] = v["value"]
+                                if i['task'] == 'T4':
+                                    for j in i["value"]:
+                                        if j['task'] == 'T5':
+                                            for l in j["value"]:
+                                                if "label" in l:
+                                                    outpObj["State"] = l["label"]
+                                        elif j['task'] == 'T6' and len( j["value"] ) > 0:
+                                            outpObj["City"] = j["value"]
+                                        elif j['task'] == 'T18' and len( j["value"] ) > 0:
+                                            outpObj["Body of water"] = j["value"]
+                                        elif j['task'] == 'T7' and len( j["value"] ) > 0:
+                                            outpObj["Country"] = j["value"]
+                                        elif j['task'] == 'T8' and len( j["value"] ) > 0:
+                                            outpObj["City"] = j["value"]
+                                        elif j['task'] == 'T19' and j["value"] != None:
+                                            outpObj["Unidentified"] = j["value"]
+                                if i['task'] == 'T14':
+                                    for j in i["value"]:
+                                        if j['task'] == 'T15':
+                                            for l in j["value"]:
+                                                if "label" in l:
+                                                    outpObj["Category"] = l["label"]
+                                        elif j['task'] == 'T12' and len( j["value"] ) > 0:
+                                            outpObj["Subject 2"] = j["value"]
+                                        elif j['task'] == 'T13' and len( j["value"] ) > 0:
+                                            outpObj["Subject 3"] = j["value"]
+                                        elif j['task'] == 'T16' and len( j["value"] ) > 0:
+                                            outpObj["Subject 1"] = j["value"]
+                                if i['task'] == 'T20':
+                                    for j in i["value"]:
+                                        if j['task'] == 'T22' and len(j["value"]) > 0:
+                                            outpObj["Other information"] = j["value"]
+                            output.append(outpObj)
+            except json.JSONDecodeError:
+                print('json parse FAIL')
+                # Move on if JSON parsing fails
+                pass
+    
+    return output, justJson
+
+csv_file_path = 'postcard-tag-old-classifications.csv'  # Replace with your CSV file path
+outputList, justJson = find_task_values(csv_file_path)
+# print(outputList)
+
+json_file_path = 'american_classify_result.json'  # Replace with the desired output JSON file path
+save_dict_to_json(outputList, json_file_path)
+
+jjson_file_path = 'american_classify_just_json.json'  # Replace with the desired output JSON file path
+save_dict_to_json(justJson, jjson_file_path)
+
+print(f"Dictionary has been saved as {json_file_path}.")
+
+
